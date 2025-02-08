057f0bd1a2585fa330308f2eb574a3894d93ae39
Ulric Atayi <155850570+Ulricatayi@users.noreply.github.com>
2025-02-09 00:48:38 +0100

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,34 +1,35 @@
-const axios = require('axios');
 module.exports.config = {
-  name: 'ai',
-  version: '1.0.0',
-  role: 0,
-  hasPrefix: false,
-  aliases: ['gpt', 'openai'],
-  description: "An AI command powered by GPT-4",
-  usage: "Ai [promot]",
-  credits: 'Men',
-  cooldown: 3,
-};
-module.exports.run = async function({
-  api,
-  event,
-  args
-}) {
-  const input = args.join(' ');
-  if (!input) {
-    api.sendMessage(`Hello how I can help you today?`, event.threadID, event.messageID);
-    return;
+  name: `ai`,
+  version: "1.1.0",
+  permission: 0,
+  credits: "Metoushela",
+  description: "",
+  prefix: false,
+  premium: false,
+  category: "without prefix",
+  usage: ``,
+  cooldowns: 3,
+  dependency: {
+    "axios": ""
   }
-  api.sendMessage(``, event.threadID, event.messageID);
-  try {
-    const {
-      data
-    } = await axios.get(`https://kaiz-apis.gleeze.com/api/gpt-3.5?q=${encodeURIComponent(input)}`);
-    const response = data.response;
-    api.sendMessage( response + , event.threadID, event.messageID);
+};
+
+module.exports.run = async function ({api, event, args}) {
+  try{
+  const axios = require('axios');
+  let ask = args.join(' ');
+  if (!ask) {
+    return api.sendMessage('✨ 𝗔𝗻𝗼𝘁𝗵𝗲𝗿-𝗠𝗲\n━━━━━━━━━━━\n\nplease provide a question.', event.threadID, event.messageID)
+  }
+
+  const res = await axios.get(`https://kaiz-apis.gleeze.com/api/gpt-4o?q=${ask}&uid=${event.senderID}`);
+  const reply = res.data.response;
+  if (res.error) {
+    return api.sendMessage('having some unexpected error while fetching api.', event.threadID, event.messageID)
+  } else {
+    return api.sendMessage(`✨ 𝗔𝗻𝗼𝘁𝗵𝗲𝗿-𝗠𝗲\n━━━━━━━━━━━\n\n${reply}\n\nby Metoushela Walker and Ulric Atayi`, event.threadID, event.messageID)
+  }
   } catch (error) {
-    api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
+    return api.sendMessage('having some unexpected error', event.threadID, event.messageID)
   }
-};
-  
+                           }
